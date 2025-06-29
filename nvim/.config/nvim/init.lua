f670a45e7563ea6bb6b514641d5721e33b4910c6
Aidan Middleton <aidanrmiddleton@gmail.com>
2025-06-29 15:30:33 -0500

    Updated neovim theme

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,171 @@
+-- ~/.config/nvim/init.lua
+
+-- require("aidan.theme").load()
+
+-- custom variables
+
+local theme = "my-theme"
+
+-- vim options
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.termguicolors = true
+
+-- Set space as leader key (optional, if not already set)
+vim.g.mapleader = " "
+
+-- Copy to clipboard
+vim.keymap.set("v", "<leader>y", '"+y', { noremap = true, silent = true })        -- Visual selection
+vim.keymap.set("n", "<leader>Y", '"+yg_', { noremap = true, silent = true })      -- From cursor to end of line
+vim.keymap.set("n", "<leader>y", '"+y', { noremap = true, silent = true })        -- Normal mode
+vim.keymap.set("n", "<leader>yy", '"+yy', { noremap = true, silent = true })      -- Yank entire line
+
+-- Paste from clipboard
+vim.keymap.set("n", "<leader>p", '"+p', { noremap = true, silent = true })        -- After cursor
+vim.keymap.set("n", "<leader>P", '"+P', { noremap = true, silent = true })        -- Before cursor
+vim.keymap.set("v", "<leader>p", '"+p', { noremap = true, silent = true })        -- Replace visual with clipboard
+vim.keymap.set("v", "<leader>P", '"+P', { noremap = true, silent = true })        -- Paste before selection
+
+
+-- Set up lazy.nvim runtime path (MUST come before require("lazy"))
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  -- Optional: auto-clone if not found
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugins
+require("lazy").setup({
+  { "neovim/nvim-lspconfig" },  -- LSP support
+  { "hrsh7th/nvim-cmp" },       -- Completion plugin
+  { "hrsh7th/cmp-nvim-lsp" },   -- LSP source for nvim-cmp
+  { "L3MON4D3/LuaSnip" },       -- Snippet engine
+  { "saadparwaiz1/cmp_luasnip" }, -- Snippet completions
+  { "nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
+  { "norcalli/nvim-colorizer.lua" },
+--  { "Mofiqul/dracula.nvim" }
+})
+
+local cmp = require("cmp")
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  }
+})
+
+local lspconfig = require("lspconfig")
+
+-- Enable pyright for Python
+lspconfig.pyright.setup{}
+
+-- Optional: diagnostics config
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+})
+
+-- Attaches to every FileType mode
+require 'colorizer'.setup()
+
+-- Attach to certain Filetypes, add special configuration for `html`
+-- Use `background` for everything else.
+require 'colorizer'.setup {
+  'css';
+  'javascript';
+  html = {
+    mode = 'foreground';
+  }
+}
+
+-- Use the `default_options` as the second parameter, which uses
+-- `foreground` for every mode. This is the inverse of the previous
+-- setup configuration.
+require 'colorizer'.setup({
+  'css';
+  'javascript';
+  html = { mode = 'background' };
+}, { mode = 'foreground' })
+
+-- Use the `default_options` as the second parameter, which uses
+-- `foreground` for every mode. This is the inverse of the previous
+-- setup configuration.
+require 'colorizer'.setup {
+  '*'; -- Highlight all files, but customize some others.
+  css = { rgb_fn = true; }; -- Enable parsing rgb(...) functions in css.
+  html = { names = false; } -- Disable parsing "names" like Blue or Gray
+}
+
+-- Exclude some filetypes from highlighting by using `!`
+require 'colorizer'.setup {
+  '*'; -- Highlight all files, but customize some others.
+  '!vim'; -- Exclude vim from highlighting.
+  -- Exclusion Only makes sense if '*' is specified!
+}
+
+
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  -- List of parsers to ignore installing (or "all")
+  ignore_install = { "javascript" },
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = {
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+    disable = { "c", "rust" },
+    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+    disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+            return true
+        end
+    end,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+
+-- Setup commands
+vim.cmd("colorscheme " .. theme)

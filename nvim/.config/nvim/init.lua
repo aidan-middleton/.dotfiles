344e69425ddacc543ac8a1156d0bde68a96e44e5
Aidan Middleton <aidanrmiddleton@gmail.com>
2025-09-05 22:40:12 -0500

    Consilidated lazy.nvim configuration

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -41,77 +41,76 @@ vim.diagnostic.config({
 -- Setup commands
 vim.cmd("colorscheme " .. theme)
 
--- [[ lazy.nvim setup ]]
+-- bootstrap lazy.nvim if not installed
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    -- Optional: auto-clone if not found
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        lazypath,
-    })
+    vim.fn.system({"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath})
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- lazy.nvim setup
 require("lazy").setup({
-    { "neovim/nvim-lspconfig"}, -- LSP support
-    { "hrsh7th/nvim-cmp"}, -- Completion plugin
+    { "neovim/nvim-lspconfig", -- LSP support
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.pyright.setup{} -- Enable pyright for Python
+        end
+    }, 
+    { "hrsh7th/nvim-cmp", -- Completion plugin
+        config = function()
+            local cmp = require("cmp")
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                }),
+                sources = {
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                }
+            })
+        end
+    }, 
     { "hrsh7th/cmp-nvim-lsp" }, -- LSP source for nvim-cmp
     { "L3MON4D3/LuaSnip" }, -- Snippet engine
     { "saadparwaiz1/cmp_luasnip" }, -- Snippet completions
-    { "nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
-    { "norcalli/nvim-colorizer.lua" },
-    { "nvim-telescope/telescope.nvim", tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' } }
-})
-
---[[ PLUGIN: neovim/nvim-lspconfig]]
-local lspconfig = require("lspconfig")
-lspconfig.pyright.setup{} -- Enable pyright for Python
-
---[[ PLUGIN: hrsh7th/nvim-cmp ]] 
-local cmp = require("cmp")
-cmp.setup({
-    snippet = {
-        expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-        end,
+    { "nvim-treesitter/nvim-treesitter", 
+        branch = 'master', lazy = false, build = ":TSUpdate",
+        config = function()
+            require 'nvim-treesitter.configs'.setup{
+                ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python", "javascript" },
+                sync_install = false,
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+            }
+        end
     },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    }),
-    sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-        { name = 'render-markdown' },
+    { "norcalli/nvim-colorizer.lua",
+        config = function()
+            require 'colorizer'.setup {
+                '*'; -- Highlight all files, but customize some others. (Note: use ! to exclude)
+                css = { rgb_fn = true; }; -- Enable parsing rgb(...) functions in css.
+            }
+        end
+    },
+    { "nvim-telescope/telescope.nvim", 
+        tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }, 
+        config = function()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+        end
     }
 })
 
---[[ PLUGIN: norcalli/nvim-colorizer.lua ]]
-require 'colorizer'.setup {
-    '*'; -- Highlight all files, but customize some others. (Note: use ! to exclude)
-    css = { rgb_fn = true; }; -- Enable parsing rgb(...) functions in css.
-}
 
-
---[[ PLUGIN: nvim-treesitter/nvim-treesitter ]]
-require 'nvim-treesitter.configs'.setup {
-    ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python", "javascript" },
-    sync_install = false,
-    auto_install = true,
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
-}
-
---[[ PLUGIN: nvim-telescope/telescope.nvim]]
-local telescope = require('telescope')
-telescope.setup{}
-local telescope_builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, { desc = 'Telescope help tags' })

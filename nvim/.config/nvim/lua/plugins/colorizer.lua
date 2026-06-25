95d905868fec47bcb4ffb0d935377a1d3c26e898
Aidan Middleton <aidanrmiddleton@gmail.com>
2026-06-24 19:18:44 -0500

    feat(nvim): expanded colorizer config to apply to all file types & many formats

--- nvim/.config/nvim/lua/plugins/colorizer.lua
+++ nvim/.config/nvim/lua/plugins/colorizer.lua
@@ -1,6 +1,25 @@
-vim.pack.add { 'https://github.com/norcalli/nvim-colorizer.lua' }
+vim.pack.add { 'https://github.com/catgoose/nvim-colorizer.lua' }
 
 require 'colorizer'.setup {
-    '*'; -- Highlight all files, but customize some others. (Note: use ! to exclude)
-    css = { rgb_fn = true; }; -- Enable parsing rgb(...) functions in css.
+    filetypes = {
+        '*', -- Highlight all files (use '!ft' to exclude a filetype).
+        -- Named colors ("red", "white") are only meaningful in stylesheets/markup,
+        -- so re-enable them just there to avoid highlighting identifiers in code.
+        css = { names = true },
+        scss = { names = true },
+        sass = { names = true },
+        less = { names = true },
+        html = { names = true },
+    },
+    user_default_options = {
+        names = false,       -- Named colors ("blue", "red") -- off by default (see filetypes).
+        RGB = true,          -- #RGB hex
+        RGBA = true,         -- #RGBA hex
+        RRGGBB = true,       -- #RRGGBB hex
+        RRGGBBAA = true,     -- #RRGGBBAA hex (with alpha)
+        rgb_fn = true,       -- rgb() and rgba() functions
+        hsl_fn = true,       -- hsl() and hsla() functions
+        css = true,          -- Enable all CSS *features* (names, RGB, RGBA, ...).
+        css_fn = true,       -- Enable all CSS *functions* (rgb_fn, hsl_fn).
+    },
 }
